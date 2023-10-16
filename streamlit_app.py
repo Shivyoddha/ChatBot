ec3486b9ed26cd0cc58f8b2c2512ad31aca10250
Anish Kumbhar <97729353+Shivyoddha@users.noreply.github.com>
2023-10-17 02:23:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('sh ./setup.sh')  # Run setup script
+os.system('sh setup.sh')  # Run setup script
 
 import subprocess
 subprocess.Popen(['python', '/text-generation-webui/server.py', '--share', '--chat', '--wbits', '4', '--groupsize', '128', '--model_type', 'llama'])

dbdb8cf026a2eac52a9574e66edf265663a5e906
Anish Kumbhar <97729353+Shivyoddha@users.noreply.github.com>
2023-10-17 02:22:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import os
 
-os.system('sh /app/setup.sh')  # Run setup script
+os.system('sh /setup.sh')  # Run setup script
 
 import subprocess
 subprocess.Popen(['python', '/app/text-generation-webui/server.py', '--share', '--chat', '--wbits', '4', '--groupsize', '128', '--model_type', 'llama'])
